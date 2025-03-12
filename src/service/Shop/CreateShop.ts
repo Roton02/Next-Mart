d5a52f2ea403261ea380ac68c5e3e4906e09af4c
Roton02 <rph645102@gmail.com>
2025-03-12 15:18:15 +0600

    solve the creadintial problem and successfuly hit the backend url and get success messages

--- src/service/Shop/CreateShop.ts
+++ src/service/Shop/CreateShop.ts
@@ -4,7 +4,6 @@ import { cookies } from 'next/headers'
 
 export const createShop = async (data: FormData) => {
   try {
-    console.log('asdfghjk', process.env.NEXT_PUBLIC_BASE_URL)
     const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/shop`, {
       method: 'POST',
       headers: {
